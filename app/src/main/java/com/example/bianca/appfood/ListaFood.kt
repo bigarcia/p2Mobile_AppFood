2c15f487617b8ede8beff71b3927db5c6e4a48ec
bigarcia <biigarciam@gmail.com>
2018-12-04 21:02:36 -0200

    Cria layout da lista de alimentos com Recycler View e o ícone do app.

---
+++ app/src/main/java/com/example/bianca/appfood/ListaFood.kt
@@ -0,0 +1,12 @@
+package com.example.bianca.appfood
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaFood : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_food)
+    }
+}
